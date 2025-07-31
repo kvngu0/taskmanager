fa22f3b52f95aba3470a011c1bafb36cbfec89c0
Kevin Nguyen <cheesy101@hotmail.com>
2025-07-31 11:52:02 +1000

    commit

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -32,7 +32,7 @@ const addTask = async (req, res) => {
 const updateTask = async = (req, res) => {
     const {title, description, completed, deadline} = req.body;
     try{
-        const task = await.Task.findById(req.params.Id);
+        const task = await Task.findById(req.params.Id);
         if (!task)
             return res.status(404).json({message:'Task not found'});
         task.title = title || title.task;
@@ -46,3 +46,19 @@ const updateTask = async = (req, res) => {
         res.status(500).json({message: error.message});
     }
 };
+
+
+// Delete Task
+const deleteTask = async(req, res) => {
+    try{
+        const task = await Task.findById(req.params.id);
+        if(!task)
+            return res.status(404).json({message: 'Task not found'});
+        await task.remove();
+        res.json({message:'Task deleted'});
+    } catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+module.exports = {getTasks, addTask, updateTask, deleteTask};

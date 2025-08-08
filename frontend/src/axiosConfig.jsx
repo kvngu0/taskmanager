cdcd41675a97129106268358fff9840390751cd9
Kevin Nguyen <cheesy101@hotmail.com>
2025-08-08 11:23:00 +1000

    redo MongoDB connection URI

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://13.210.187.0',
+  //baseURL: 'https://13.210.187.0',
   
-  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://localhost:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
